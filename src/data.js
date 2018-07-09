96949558c1db6467c083aff5ab568542db14eff1
ViniciaLaraRangel <39544010+ViniciaLaraRangel@users.noreply.github.com>
2018-07-09 15:20:55 -0500

    Subiendo MVP 9Jul

--- src/data.js
+++ src/data.js
@@ -1,46 +1,154 @@
-//Este es el código en FETCH, comenzar a enlazar con window data y main para cargar el JSON
-window.data = {
-    cargarData: () => {
-        let laboratoriaJSON = fetch('https://github.com/ViniciaLaraRangel/cdmx-2018-06-bc-core-am-data-dashboard/blob/master/data/laboratoria.json');
-//Hacer que la data sea tomada por las funciones
-        data.computeStudentsStats(laboratoriaJSON);
-//Usar coma para cerrar    
-    },
-//Crear la promesa para la primera función 
-    computeStudentsStats: (laboratoriaJSON) => {
-        laboratoria.then(function(res){
-            return res.json();
+// Guardar en una variable la liga del JSON desde el servidor GitHub
+const url = 'https://raw.githubusercontent.com/ViniciaLaraRangel/cdmx-2018-06-bc-core-am-data-dashboard/javascriptFetch/data/laboratoria.json';
+// Crear método global para hacer FETCH y que la data se cargue para tranformarlo en el argumento result (data que se va a pasar)
+window.getData = () => {
+  fetch(url)
+    .then(result => result.json())
+    .then(result => {
+      // console.log(result);
+      const students = computeStudentsStats(result);
+      printInfo(students);
+      printSearch(students);
+      filterStudent(students);
+      printSearchCampus(students);
+      filterGeneration(students);
     })
-        .then(function(data){
-            let sedes = Object.getOwnPropertyNames(data);
-            let generaciones = Object.values(data);
-            console.log(sedes);
-//Aquí acaba el primer nivel
-                generaciones.forEach(function(generacionesSeg){
-                    let generacionesInfo = Object.values(generacionesSeg.generacion);
-                    generacionesInfo.forEach(function(estudiantesSegundo){
-                        let estudiantesInfo = estudiantesSegundo.estudiantes;
-                        estudiantesInfo.forEach(function(avanceTercer){
-                            let porcentajeCompl = avanceTercer.progreso;
-                            let nombres = avanceTercer.nombre;
-                                console.log(nombres);
-                        })
-                })
-            })
+    .catch(error => {
+      // console.log('error');
+    });
+};
 
-    })
-//Usar coma para cerrar
+window.addStats = progress => {
+  // console.log(progress);
+  // Crear objeto que contenga porcentaje completado con una propiedad topics
+  let stats = {};
+  let topicsArray = progress.temas;
+  // console.log(topicsArray);
+
+  stats.completedPercentage = progress.porcentajeCompletado;
+  stats.topics = topicsArray;
+  // console.log(topicsArray);
+
+  // Hacer bucle para que le asigne un status a cada estudiante de acuerdo a su completitud
+  if (progress.porcentajeCompletado < 60) {
+    stats.status = 'bajo';
+  }
+  if (progress.porcentajeCompletado > 60 && progress.porcentajeCompletado < 90) {
+    stats.status = 'medio';
+  } else {
+    stats.status = 'alto';
+  }
+
+  let properties = Object.values(progress.temas);
+  // console.log(properties);
+  properties.forEach(contentPeer => {
+    contentPeer.completedPercentage = contentPeer.porcentajeCompletado;
+    // Crear una nueva propiedad que se llame percentageDuration (DuracionTemaCompletado x 100 / duracion de tema)
+    // console.log(contentPeer.completedPercentage);
+    contentPeer.percentageDuration = Math.round((contentPeer.duracionTemaCompletado * 100) / contentPeer.duracionTema);
+    // console.log(contentPeer);
+    let subTopicsArray = contentPeer.subtemas;
+    let subProperties = Object.values(subTopicsArray);
+    subProperties.forEach(subContentPeer => {
+      subContentPeer.type = subContentPeer.tipo;
+      // Propiedad de subtema
+      subContentPeer.duration = subContentPeer.duracionSubtema;
+      if (subContentPeer.completado === 1) {
+        subContentPeer.completedPercentage = 100;
+      } else {
+        subContentPeer.completedPercentage = 0;
+      }
+    });
+  });
+  // console.log(stats);
+  return stats;
+};
+
+
+// (En dónde se va a recibir) Parámetro Laboratoria, que dice el readme
+window.computeStudentsStats = laboratoria => {
+  let studentArray = [];
+  // Usar método para que se puedan obtener los nombres de las propiedades (keys) de los objetos en un array
+  let campus = '';
+  let generationArray = '';
+  let allGenerationsContent = '';
+  let i = 0;
+  let j = 0;
+
+  campus = Object.getOwnPropertyNames(laboratoria);
+  generationArray = Object.values(laboratoria);
+  // console.log(generationArray);
+  // Iteración al primer nivel de data que arroja la cantidad de generaciones por campus
+  generationArray.forEach(element => {
+    allGenerationsContent = Object.values(element.generacion);
+    allGeneration = Object.keys(element.generacion);
+    j = 0;
+    allGenerationsContent.forEach(student => {
+      let students = student.estudiantes;
+      students.forEach(infoStudent => {
+        // console.log(infoStudent.progreso);
+        let stats = addStats(infoStudent.progreso);
+        // console.log(infoStudent.progreso);
+        // llenar el arreglo student
+        studentArray.push({
+          'campus': campus[i],
+          'generation': allGeneration[j],
+          'name': infoStudent.nombre,
+          'email': infoStudent.correo,
+          'turn': infoStudent.turno,
+          'stats': stats
+        });
+      });
+      j++;
+    });
+    i++;
+  });
+  // console.log(studentArray);
+  return studentArray;
 },
 
-    computeGenerationsStats: () => {
+// computeGenerationsStats: () => {
+window.computeGenerationStats = (student, search) => {
 
-    },
+},
 
-    sortStudents: () => {  
+// window.sortStudents: () => {
 
+// },
+
+window.filterStudent = (student, search) => {
+  let searchResult = [];
+  student.forEach(element => {
+    if (element.name.indexOf(search) !== -1) {
+      searchResult.push(element);
     }
+  });
+  // console.log(element);
+  return searchResult;
+};
 
-    filterStudents: () => {
-
+// Filtrar Sedes y generaciones
+window.filterCampus = (student, search) => {
+  let searchResult = [];
+  student.forEach(element => {
+    // console.log(element);
+    if (element.campus === search) {
+      searchResult.push(element);
     }
+  });
+  // console.log(element);
+  return searchResult;
+};
+
+window.filterGeneration = (student, search) => {
+  let searchResult = [];
+  // console.log(student);
+  student.forEach(element => {
+    // console.log(search);
+    if (element.generation === search) {
+      searchResult.push(element);
+    }
+  });
+  // console.log(searchResult);
+  return searchResult; 
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,90 @@
-//Enlazar el JSON y las funciones de data.js con este manejo del DOM
-data.cargarData();
+// Traer de data.js
+const printInfo = students => {
+  // console.log(students);
+};
 
-//Declarar lo que se va a imprimir en el DOM
-let sede1 = document.getElementById("sede");
-let buton = document.getElementById("buton");
+// Para imprimir en el DOM hardcodeando
+// let sede1 = document.getElementById('sede');
+// let buton = document.getElementById('buton');
+
+const printSearch = students => {
+  document.getElementById('search').addEventListener('click', (event) => {
+    event.preventDefault(); 
+    const name = document.getElementById('name-to-search').value;
+    if (name !== '' && name !== ' ') {
+      const searchResult = filterStudent(students, name);
+      let result = '';
+      if (searchResult.length === 0) {
+        document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+      } else {
+        searchResult.forEach((element, i) => {
+          result += `<tr>
+                      <th scope="row">${i + 1}</th>
+                      <td>${element.name}</td>
+                      <td>${element.generation}</td>
+                      <td>${element.stats.completedPercentage}%</td>
+                      <td>${element.campus}</td>
+                    </tr>`;
+        });
+        document.getElementById('no-result').innerHTML = '';
+        document.getElementById('table-body').innerHTML = result;
+      }
+    }
+  });
+};
+
+const printSearchCampus = students => {
+  document.getElementById('botonResultado').addEventListener('click', (event) => {
+    event.preventDefault();
+    const sede = document.getElementById('sedesBuscar').value;
+    const generacion = document.getElementById('generacionesBuscar').value;
+    // console.log(sede);
+    // console.log(generacion);
+    const searchResult = filterCampus(students, sede);
+    const searchResultGen = filterGeneration(searchResult, generacion);
+    // console.log(searchResultGen);
+    // Para imprimir en el DOM
+    let result = '';
+    if (searchResult.length === 0) {
+      document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+    } else {
+      searchResultGen.forEach((element, i) => {
+        result += `<tr>
+                    <th scope="row">${i + 1}</th>
+                    <td>${element.name}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.stats.completedPercentage}%</td>
+                    <td>${element.campus}</td>
+                  </tr>`;
+      });
+      document.getElementById('no-result').innerHTML = '';
+      document.getElementById('table-body').innerHTML = result;
+    }
+  });
+};
+
+const printSearchGeneration = students => {
+  document.getElementById('botonResultado').addEventListener('click', (event) => {
+    event.preventDefault();
+    const generacionFiltrar = document.getElementById('generacionesBuscar').value;
+    console.log(generacionFiltrar);
+    const searchResult = filterCampus(students, generacionFiltrar);
+    // Para imprimir en el DOM
+    let result = '';
+    if (searchResult.length === 0) {
+      document.getElementById('no-result').innerHTML = '<p>No existen resultados</p>';
+    } else {
+      searchResult.forEach((element, i) => {
+        result += `<tr>
+                    <th scope="row">${i + 1}</th>
+                    <td>${element.name}</td>
+                    <td>${element.generation}</td>
+                    <td>${element.stats.completedPercentage}%</td>
+                    <td>${element.campus}</td>
+                  </tr>`;
+      });
+      document.getElementById('no-result').innerHTML = '';
+      document.getElementById('table-body').innerHTML = result;
+    }
+  });
+};
